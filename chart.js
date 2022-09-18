01fffe4abaa155d6584c9650ab2deb9a7037b22d
sparrishmatt <106349010+sparrishmatt@users.noreply.github.com>
2022-09-17 19:05:39 -0500

    Update chart.js

--- chart.js
+++ chart.js
@@ -63,7 +63,7 @@ function buildCharts(sample) {
         //  5. Create a variable that holds the first sample in the array.
 
   var specSampArr = samples.filter(
-    (sample) => sample.id == sample);
+    (sampleobject) => sampleobject.id == sample);
 
   var specSamp = specSampArr[0];
   console.log(specSamp)
